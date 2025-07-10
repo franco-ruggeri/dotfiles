033f720c5a808b080038c9123fc374a254eff98f
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-10 10:30:33 +0000

    chore(nvim): remove all core stuff (only lazy)

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,14 +1,8 @@
 local M = {}
 
-M.options = require("core.options")
-M.keymaps = require("core.keymaps")
-M.autocmds = require("core.autocmds")
 M.plugins = require("core.plugins")
 
 M.setup = function()
-	M.options.setup()
-	M.keymaps.setup()
-	M.autocmds.setup()
 	M.plugins.setup()
 end
 
