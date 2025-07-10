4e944b6723b026e64167eb749a546ac4059bd399
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-10 12:45:29 +0000

    chore: remove keymaps

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,12 +1,10 @@
 local M = {}
 
 M.options = require("core.options")
-M.keymaps = require("core.keymaps")
 M.plugins = require("core.plugins")
 
 M.setup = function()
 	M.options.setup()
-	M.keymaps.setup()
 	M.plugins.setup()
 end
 
